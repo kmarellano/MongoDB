c68535bbbda96e7eb0742cc756d4672a11c62ccb
kmarellano <arellanokurt0130@gmail.com>
2023-09-07 08:53:55 +0800

    model cleanup

--- models/index.js
+++ models/index.js
@@ -1,31 +1,32 @@
 const cAccountRouting = require("./caccountrouting.model");
 const cAccounts = require("./caccounts.model");
 const cAddressLinks = require("./caddresslinks.model");
-const cardEmbossing = require("./cardembossing.model");
 const cardStatusX = require("./cardstatusx.model");
 const cardX = require("./cardx.model");
 const cExtension = require("./cextension.model");
-const countries = require("./countries.model");
-const cStatementLinks = require("./cstatementlinks.model");
-const cStatements = require("./cstatements.model");
 const cTransactions = require("./ctransactions.model");
-const products = require("./products.model");
-const riskDomains = require("./riskdomains.model");
-const trxnTypes = require("./trxntypes.model");
+
+// const cStatements = require("./cstatements.model");
+// const cardEmbossing = require("./cardembossing.model");
+// const countries = require("./countries.model");
+// const cStatementLinks = require("./cstatementlinks.model");
+// const products = require("./products.model");
+// const riskDomains = require("./riskdomains.model");
+// const trxnTypes = require("./trxntypes.model");
 
 module.exports = {
   cAccountRouting,
   cAccounts,
   cAddressLinks,
-  cardEmbossing,
   cardStatusX,
   cardX,
   cExtension,
-  countries,
-  cStatementLinks,
-  cStatements,
   cTransactions,
-  products,
-  riskDomains,
-  trxnTypes,
+  // cardEmbossing,
+  // countries,
+  // cStatementLinks,
+  // cStatements,
+  // products,
+  // riskDomains,
+  // trxnTypes,
 };
